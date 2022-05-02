052436c6f437f8ce22704109f191c61beb9d0c73
ahmed-razic <ahmed.razic@gmail.com>
2022-05-02 02:00:32 +0200

    get posts, configurate babel

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-//import { http } from './http';
+import 'regenerator-runtime/runtime';
+import { http } from './http';
+import { ui } from './ui';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
-    .then((data) => console.log(data))
+    .then((data) => console.log(ui.showPosts(data)))
     .catch((err) => console.log(err));
 }
